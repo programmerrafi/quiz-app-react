9ae4be09872f4e0396fb0f4ffbb30f9e9559844d
Md Rafi <mdrafiprogrammer@gmail.com>
2022-08-21 19:01:34 +0600

    React-quiz

--- src/components/Context/firebase.js
+++ src/components/Context/firebase.js
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 
 // Firebase configuration
 const app = initializeApp({
-  apiKey: "AIzaSyB4VqPBC_ov2wX2gpiTGbOghqWoCfyhJQA",
-  authDomain: "quiz-app-ab56a.firebaseapp.com",
-  databaseURL:
-    "https://quiz-app-ab56a-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "quiz-app-ab56a",
-  storageBucket: "quiz-app-ab56a.appspot.com",
-  messagingSenderId: "939902121987",
-  appId: "1:939902121987:web:f88dc265d969a7bd1f3636",
+  apiKey: "AIzaSyB9RChq9lIF0QDtJZmkB2-g7hPZ787dFfg",
+  authDomain: "quiz-app-test-aaa5b.firebaseapp.com",
+  databaseURL: "https://quiz-app-test-aaa5b-default-rtdb.firebaseio.com",
+  projectId: "quiz-app-test-aaa5b",
+  storageBucket: "quiz-app-test-aaa5b.appspot.com",
+  messagingSenderId: "153337502745",
+  appId: "1:153337502745:web:7a0b810f61a75312e41e88",
 });
 
 export default app;
